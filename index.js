545eb4ed97cd06ab7af2e85a3f97b07d1c277aab
Kyrylo <kirill26031@gmail.com>
2020-09-20 16:23:04 +0300

    Changed API for better recognition 'partOfSpeech', added patterns.json

--- index.js
+++ index.js
@@ -1,43 +1,83 @@
 require('dotenv').config()
 const fetch = require('node-fetch');
-var unirest = require('unirest');
+const fs = require('fs')
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const patterns = JSON.parse(fs.readFileSync('./patterns.json'))
 
 bot.start((ctx) => ctx.reply("Hi!"))
 
 bot.on('text', (ctx) => {
     console.log(ctx.update.message.text)
-    answer(ctx.update.message.text).then(res => ctx.reply(res))
+    // answer(ctx.update.message.text).then(res => ctx.reply(res))
+    answer(ctx.update.message.text).then(res => {
+        console.log(res)
+        ctx.reply(res)
+    })
 })
 
 bot.launch()
 
-const answer = async (message) =>{
-    let m = message.toLowerCase().split(' ')
-    m.forEach(word => {wordTypes(word).then(arr=>{
-        console.log(word)
-        // console.log(arr)
-    })})
-    return JSON.stringify(m, null, 4)
+const answer = async (message) => {
+    let pat = /\/W+/
+    message = message.toLowerCase()
+    let m = message.split(' ')
+    let words = await Promise.all(
+        m.map(word => { return wordTypes(word.replace(pat, '')) })
+    ).then(values => {
+        o_v = []
+        values.forEach(w => o_v.splice(m.indexOf(w.text), 0, w))
+        return o_v
+    })
+    .then(arr => {
+        let str = ""
+        arr.forEach(w => {
+            str = str+w.text+'-('
+            w.parts.forEach(p => str = str+'/'+p)
+            str = str +') '
+        })
+        return str
+    })
+    console.log(words)
+    // return words
+    for (key in patterns.Patterns) {
+        if (patterns.Patterns.hasOwnProperty(key)) {
+            if(words.match(key)) return getRandom(patterns.Patterns[key])
+        }
+    }
+    return "Error"
+
 }
 
 const wordTypes = async (word) => {
-    const response = await fetch("https://wordsapiv1.p.rapidapi.com/words/" + word + "/definitions/", {
+    return await fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/" + word, {
         headers: {
-            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-host": "lingua-robot.p.rapidapi.com",
             "x-rapidapi-key": process.env.API_TOKEN,
             "useQueryString": true
         }
     }).then(res => res.json())
-    .then(res => {
-        if(res.success === false) throw new Error("Unknown word")
-        return res.definitions.map(def => def.partOfSpeech).filter((v, i, a) => a.indexOf(v) === i)
-    }).then(res => JSON.stringify(res, null, 4))
-    .catch(err => {
-        console.log(err)
-        return null
-    })
-    console.log(response)
-    return response
+        .then(res => {
+            if (res.success === false) throw new Error("Unknown word")
+            let arr = []
+            const aux_regex = /\(auxiliary\)/
+            res.entries.forEach(entry => {
+                entry.interpretations.forEach(inter => arr.push(inter.partOfSpeech))
+                entry.lexemes.forEach(lexeme => lexeme.senses.forEach(sense => {
+                    if(sense.definition.match(aux_regex)) arr.push('auxiliary')
+                }))
+            })
+            return {
+                text: word,
+                // entries: res.entries.map(entry => entry.entry),
+                parts: arr.filter((v, i, a) => a.indexOf(v) === i)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            return null
+        })
 }
+
+const getRandom = (arr) => arr[Math.floor(arr.length*Math.random())]
