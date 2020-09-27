74797224a9b30badb45797c7ec532a8b7bbaa1d2
LordNani <rigansu@gmail.com>
2020-09-27 20:31:59 +0300

    Deleted extra

--- api.js
+++ api.js
@@ -17,6 +17,19 @@ const getGender = async (name, cache) => {
         return res
     })
 }
+
+async function getRandomFact() {
+    return fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then(res => res.json());
+} 
+
 module.exports = {
-    getGender
+    getGender,getRandomFact
 }
+
+// if(message.includes("fact")){
+//     resultArray.push(genericPhrase[Math.floor(Math.random()*resultArray.length)])
+//     resultArray.push(await facts.getRandomFact().then(res => {return res.text}))
+    
+//     return resultArray
+// }
